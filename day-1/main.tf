5bdc45d0b76ad5714db23f740892399734dddf14
mallikarjun <mallikarjun.vtalent@gmail.com>
2025-07-08 09:09:54 +0530

    added

--- day-1/main.tf
+++ day-1/main.tf
@@ -5,5 +5,14 @@ resource "aws_instance" "name" {
 
 }
 resource "aws_s3_bucket" "name" {
-  bucket = "mynewbucketfromterrafrom"
+  bucket = "mynewbucketfromterrafrom1111"
+}
+resource "aws_instance" "dev3" {
+  ami = "ami-05ffe3c48a9991133"
+  instance_type = "t2.micro"
+  key_name = "test"
+  tags = {
+    name = "dev"
+  }
+
 }
