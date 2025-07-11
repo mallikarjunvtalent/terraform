d444fec7f506e59b3ebee797ee4edfacebe791bc
mallikarjun <mallikarjun.vtalent@gmail.com>
2025-07-11 07:08:29 +0530

    day5

--- day-5-statefile-remote/main.tf
+++ day-5-statefile-remote/main.tf
@@ -10,6 +10,10 @@ resource "aws_vpc" "name" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_vpc" "name2" {
+  cidr_block = "10.0.0.0/24"
+}
+
 # resource "aws_instance" "ec2-2" {
 #   ami = var.ami_id
 #   instance_type = var.instance_type
